1302691348fa077a90d0db4b3dc2748da8dc7362
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-03-19 21:54:43 +0800

    get started

---
+++ app/ui/main-logo.tsx
@@ -0,0 +1,12 @@
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+export default function MainLogo() {
+  return (
+    <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`}>
+      <div className="mr-2">
+        <Image src="/main-logo.png" alt="Main Logo" width={500} height={500} />
+      </div>
+    </div>
+  );
+}
