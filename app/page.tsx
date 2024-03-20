95a9d362fbe2305002333912fdab113c9c86227f
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-03-20 08:22:14 +0800

    updateUI

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import MainLogo from "./ui/main-logo";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between p-5 bg-white">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
       <MainLogo/>
         <div className="flex items-center">
          
-          <h1 className="ml-2 text-3xl font-bold">Your Website Title</h1>
+          <h1 className="ml-2 text-3xl font-bold text-black	">Your Website Title</h1>
         </div>
       </div>
     </main>
--- app/ui/main-logo.tsx
+++ app/ui/main-logo.tsx
@@ -5,7 +5,7 @@ export default function MainLogo() {
   return (
     <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`}>
       <div className="mr-2">
-        <Image src="/main-logo.png" alt="Main Logo" width={500} height={500} />
+        <Image src="/main-logo.png" alt="Main Logo" width={200} height={200} />
       </div>
     </div>
   );
